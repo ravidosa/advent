e2491eeec157ebccf6febe729fc076764a023aa8
ravi <55117331+ravidosa@users.noreply.github.com>
2024-12-23 06:06:10 +0000

    .

--- 2024/23a.py
+++ 2024/23a.py
@@ -1,4 +1,3 @@
-import itertools
 import networkx as nx
 f = open("2024/input-23.txt", "r")
 inp = f.read().split("\n")
@@ -8,11 +7,11 @@ for i in inp:
     u, v = i.split("-")
     G.add_edge(u, v)
 s = 0
-cl = set()
-for triple in itertools.combinations(G.nodes, 3):
-    a, b, c = triple
-    if (a, b) in G.edges and (b, c) in G.edges and (a, c) in G.edges:
+for clique in nx.enumerate_all_cliques(G):
+    if len(clique) == 3:
+        a, b, c = clique
         if a[0] == "t" or b[0] == "t" or c[0] == "t":
             s += 1
-            cl.add(triple)
+    elif len(clique) > 3:
+        break
 print(s)
